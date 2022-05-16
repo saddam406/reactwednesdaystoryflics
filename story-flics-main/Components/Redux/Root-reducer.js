7bf6fa45def2d82662b8daaab609ec0f176fa0f7
saddam406 <99323581+saddam406@users.noreply.github.com>
2022-05-16 11:14:06 +0530

    delete

--- story-flics-main/Components/Redux/Root-reducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import reducers from "./Reducer";
-const RootReducer = combineReducers({data: reducers,});
-
-export default RootReducer;
--- story-flics-main/Components/Redux/Store.js
+++
@@ -1,8 +0,0 @@
-import {applyMiddleware,createStore} from 'redux';
-
-import RootReducer from './Root-reducer';
-import reduxThunk from 'redux-thunk';
-const middlewares=[reduxThunk];
-
-const store = createStore(RootReducer, applyMiddleware(...middlewares));
-export default store;
